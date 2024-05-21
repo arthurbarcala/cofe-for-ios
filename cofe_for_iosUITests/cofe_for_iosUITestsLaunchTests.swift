9cceffd16f09b421df0c0e148f5dd22c937d0828
arthurbarcala <arthurfbarcalars@gmail.com>
2024-05-21 16:57:23 -0300

    Initial Commit

---
+++ cofe_for_iosUITests/cofe_for_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  cofe_for_iosUITestsLaunchTests.swift
+//  cofe_for_iosUITests
+//
+//  Created by Arthur Silva on 21/05/24.
+//
+
+import XCTest
+
+final class cofe_for_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

f6be6d7b009c2bf4fa35d6cb5698b9f22ae5bfb0
arthurbarcala <arthurfbarcalars@gmail.com>
2024-05-23 17:10:25 -0300

    adding cofe interface

---
+++ Pods/Target Support Files/Pods-cofe_for_ios-cofe_for_iosUITests/Pods-cofe_for_ios-cofe_for_iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cofe_for_ios_cofe_for_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cofe_for_ios_cofe_for_iosUITests
+@end
--- cofe_for_ios/ViewController.swift
+++ cofe_for_ios/ViewController.swift
@@ -1,19 +1,80 @@
-//
-//  ViewController.swift
-//  cofe_for_ios
-//
-//  Created by Arthur Silva on 21/05/24.
-//
-
-import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var cofeTitle = UILabel(frame: .zero)
+    lazy var coffeeMaker = UILabel(frame: .zero)
+    lazy var loginButton = UIButton(frame: .zero)
+    lazy var credits = UILabel(frame: .zero)
+    
+    private func setupView() {
+        cofeTitle.text = "Cofe for iOS ☕️"
+        cofeTitle.textAlignment = .center
+        cofeTitle.font = UIFont(name: "HelveticaNeue-Bold", size: 40)
+        cofeTitle.textColor = .white
+        self.view.addSubview(cofeTitle)
+        
+        coffeeMaker.text = "Alguém vai fazer o nosso cafézinho hoje! 😃"
+        coffeeMaker.textAlignment = .center
+        coffeeMaker.font = UIFont(name: "HelveticaNeue", size: 20)
+        coffeeMaker.textColor = .white
+        coffeeMaker.numberOfLines = 0
+        self.view.addSubview(coffeeMaker)
+        
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = .white
+        loginButton.setTitleColor(.black, for: .normal)
+        loginButton.layer.cornerRadius = 5
+        self.view.addSubview(loginButton)
+        
+        credits.text = "Made with ❤️ by tutu."
+        credits.textAlignment = .center
+        credits.font = UIFont(name: "HelveticaNeue", size: 15)
+        credits.textColor = .white
+        self.view.addSubview(credits)
+        
+    }
+    
+    private func setupConstraints() {
+        cofeTitle.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.height.equalTo(300)
+            make.width.equalTo(300)
+        }
+        
+        coffeeMaker.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(cofeTitle.snp.bottom)
+            make.height.equalTo(300)
+            make.width.equalTo(300)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(coffeeMaker.snp.bottom).offset(50)
+            make.height.equalTo(50)
+            make.width.equalTo(100)
+        }
+        
+        credits.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-20)
+            make.height.equalTo(20)
+            make.width.equalTo(300)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor(red: 145/255, green: 152/255, blue: 229/255, alpha: 1.0)
+        
+        setupView()
+        setupConstraints()
+        
     }
-
-
+    
 }
+
 
